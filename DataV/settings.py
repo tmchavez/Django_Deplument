6983316a99d9f03b38c5c9250e5a1536796c3b1f
tmchavez <42500309+tmchavez@users.noreply.github.com>
2018-12-21 13:06:48 -0800

    Update settings.py

--- DataV/settings.py
+++ DataV/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_a%$-1#4(ybm_uas+6%cp#_uljie6aw5#y9iunn*(u)za+#0v^'
+#SECRET_KEY = '_a%$-1#4(ybm_uas+6%cp#_uljie6aw5#y9iunn*(u)za+#0v^'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
